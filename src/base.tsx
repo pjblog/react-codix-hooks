6b559a915472fafba47ff7cb7681a0305751e0bb
沈赟杰 <shenyunjie@znrank.com>
2023-01-19 16:39:54 +0800

    update

--- src/base.tsx
+++ src/base.tsx
@@ -2,7 +2,7 @@ import type { AxiosRequestConfig } from 'axios';
 import { createContext, PropsWithChildren, useContext } from "react";
 import { createDefaultBlogConfigsValue, getBlogConfigs, getBlogThemeConfigs, IConfigs } from '@pjblog/axios';
 import { useAsync } from '@codixjs/fetch';
-import { ErrorBoundary } from './error';
+import { ErrorBoundary, useBlogErrorRenderContext } from './error';
 import { usePolyfill } from "./polyfill";
 
 const BlogConfigsContext = createContext(createDefaultBlogConfigsValue());
@@ -18,7 +18,9 @@ export function useBlogThemeConfigsContext<T = any>() {
 
 export function BlogBaseConfigsProvider<T = any, U extends IConfigs = IConfigs>(props: PropsWithChildren<{}>) {
   const options = usePolyfill();
+  const ErrorComp = useBlogErrorRenderContext();
   const { data: [ configs, themeConfigs ], error } = useAsync('base', () => getBaseRequest<T, U>(options));
+  if (configs.close) return <ErrorComp code={499} message="Website closed" />
   return <ErrorBoundary error={error}>
     <BlogConfigsContext.Provider value={configs}>
       <BlogThemeConfigsContext.Provider value={themeConfigs}>
